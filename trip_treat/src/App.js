642649b9eb23e5532ed1e0882dbe1b59ec803f56
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-07-03 23:12:29 -0400

    trip_

---
+++ trip_treat/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Trip & Treat</h1>
+      <img src= {"logo.svg"} className="App-logo" alt="logo" style ={{
+        width: "100px",
+        height: "100px"
+
+      }} />
+    </div>
+  );
+}
+
+export default App;
