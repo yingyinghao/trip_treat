37bc70d641485f98fdbc3de4bb7576529baebf90
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-07-06 10:32:24 -0400

    List

--- trip_treat/src/components/List/List.jsx
+++ trip_treat/src/components/List/List.jsx
@@ -6,7 +6,8 @@ import useStyles from "./styles.js";
 
 const List = () => {
   const classes = useStyles();
-
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
 
   return (
 <div className = {classes.container}>
@@ -19,6 +20,18 @@ const List = () => {
       <MenuItem value="attractions">Attractions</MenuItem>
     </Select>
   </FormControl>
+
+
+ <FormControl className={classes.formControl}>
+    <InputLabel>Rating</InputLabel>
+    <Select value={type} onChange={(e) => setType(e.target.value)}>
+      <MenuItem value="{0}">All</MenuItem>
+      <MenuItem value="{3}">Above 3.0</MenuItem>
+      <MenuItem value="{4}">Above 4.0</MenuItem>
+      <MenuItem value="{4.5}">Above 4.5</MenuItem>
+    </Select>
+  </FormControl>
+
 
 </div>
 
