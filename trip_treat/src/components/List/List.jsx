1875cd9c3f0ba413c0e57aa2fb7de3916e6b3ada
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-07-05 23:37:00 -0400

    List

--- trip_treat/src/components/List/List.jsx
+++ trip_treat/src/components/List/List.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { CircularProgress, Grid, Typography } from "@material-ui/core";
+
+
+import useStyles from "./styles.js";
 
 const List = () => {
+  const classes = useStyles();
+
+
   return (
-      <h1>List</h1>
+<div className = {classes.container}>
+  <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+  <FormControl className={classes.formControl}>
+    <InputLabel>Type</InputLabel>
+    <Select value={type} onChange={(e) => setType(e.target.value)}>
+      <MenuItem value="restaurants">Restaurants</MenuItem>
+      <MenuItem value="hotels">Hotels</MenuItem>
+      <MenuItem value="attractions">Attractions</MenuItem>
+    </Select>
+  </FormControl>
+
+</div>
+
+
   );
 }
 
